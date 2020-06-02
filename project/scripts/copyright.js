e4893e460ea378378bfef24b7db665b0297984b9
John Sudds <jsuddsjr@hotmail.com>
2020-06-01 23:14:13 -0700

    W7 assignment finish

--- project/scripts/copyright.js
+++ project/scripts/copyright.js
@@ -1,3 +1,7 @@
 (function(d) {
     document.getElementById("copyright-year").innerText = d.getFullYear();
 })(new Date);
+
+(function(className) {
+    document.getElementsByClassName(className)[0].tabIndex = -1;
+})('active');
