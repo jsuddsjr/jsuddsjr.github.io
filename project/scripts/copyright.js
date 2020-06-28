9085b84c55570d9f735df533e2831cec2c171529
John Sudds <jsuddsjr@hotmail.com>
2020-06-27 21:14:35 -0700

    Form reservations

--- project/scripts/copyright.js
+++
@@ -1,7 +0,0 @@
-(function(d) {
-    document.getElementById("copyright-year").innerText = d.getFullYear();
-})(new Date);
-
-(function(className) {
-    document.getElementsByClassName(className)[0].tabIndex = -1;
-})('active');
---
+++ project/scripts/helper-scripts.js
@@ -0,0 +1,37 @@
+(function(d) {
+    document.getElementById("copyright-year").innerText = d.getFullYear();
+})(new Date);
+
+(function(className) {
+    document.getElementsByClassName(className)[0].tabIndex = -1;
+})('active');
+
+(function(startId, endId) {
+    let endInput = document.getElementById(endId);
+    let startInput = document.getElementById(startId);
+    const toDateOnly = (d) => d.toISOString().substring(0, 10);
+
+    if (endInput && startInput) {
+        startInput.setAttribute("min", toDateOnly(new Date()))
+        startInput.addEventListener('input', (e) => {
+            let dt = new Date(e.target.value);
+            dt.setDate(dt.getDate() + 1);
+            endInput.setAttribute("min", toDateOnly(dt))
+            endInput.disabled = false;
+        });
+        startInput.addEventListener('keypress', (e) => {
+            e.preventDefault();
+            return false;
+        })
+        endInput.addEventListener('input', (e) => {
+            let dt = new Date(e.target.value);
+            dt.setDate(dt.getDate() - 1);
+            startInput.setAttribute("max", toDateOnly(dt))
+        })
+        endInput.addEventListener('keypress', (e) => {
+            e.preventDefault();
+            return false;
+        })
+    }
+
+})('CheckIn', 'CheckOut');
