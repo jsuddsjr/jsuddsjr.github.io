4b99a1db0c71ba6d97eb04cd8b175f3681455179
John Sudds <jsuddsjr@hotmail.com>
2020-10-03 12:36:46 -0700

    Content

--- lesson3/js/updateTime.js
+++ lesson3/js/updateTime.js
@@ -1,5 +1,11 @@
 (function () {
   const d = new Date(document.lastModified);
-  document.getElementById("year").innerText = d.getFullYear();
-  document.getElementById("time").innerText = d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
+  document.getElementById("year").textContent = d.getFullYear();
+  document.getElementById("time").textContent =
+    d.toLocaleDateString() + " " + d.toLocaleTimeString();
+
+  window.setInterval(
+    () => (document.getElementById("date").textContent = new Date()),
+    1000
+  );
 })();
