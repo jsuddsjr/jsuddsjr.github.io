d28fa26831be49e5e2b494d14e19ef11e1a7f865
John Sudds <jsuddsjr@hotmail.com>
2020-10-07 20:08:12 -0700

    Accessible menu

--- lesson4/js/menu.js
+++ lesson4/js/menu.js
@@ -1,8 +1,24 @@
 (function () {
   const hamburger = document.getElementById("hamburger");
+  const menuState = document.getElementById("menu-state");
+
+  // Selecting a menu item closes the menu.
   document
     .querySelectorAll("nav a")
-    .forEach((el) => el.addEventListener("click", (e) => {
-        hamburger.checked = false;
-    }));
+    .forEach((el) =>
+      el.addEventListener("click", (e) => (hamburger.checked = false))
+    );
+
+  menuState.addEventListener("keypress", (e) => {
+    if (e.  key === "Enter" || e.key === " ") {
+      hamburger.checked = !hamburger.checked;
+      e.preventDefault();
+    }
+  });
+
+  // Update expand state when checkbox changes.
+  hamburger.addEventListener(
+    "change",
+    (e) => (menuState.ariaExpanded = hamburger.checked)
+  );
 })();
