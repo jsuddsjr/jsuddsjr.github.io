968916db35b51c9d4615e97b81cc32b1dc3e010e
John Sudds <jsuddsjr@hotmail.com>
2020-10-07 22:36:49 -0700

    Menu effects

--- lesson4/js/menu.js
+++ lesson4/js/menu.js
@@ -1,16 +1,28 @@
 (function () {
   const hamburger = document.getElementById("hamburger");
   const menuState = document.getElementById("menu-state");
+  const nav = document.querySelector("nav");
 
   // Selecting a menu item closes the menu.
-  document
-    .querySelectorAll("nav a")
-    .forEach((el) =>
-      el.addEventListener("click", (e) => (hamburger.checked = false))
-    );
+  document.querySelectorAll("nav a").forEach((el) => {
+    el.addEventListener("click", (e) => (hamburger.checked = false));
+    el.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        hamburger.checked = false;
+      }
+    });
+  });
 
+  // Collapse menu if focus leaves.
+  document.body.addEventListener("focusin", (e) => {
+    if (!nav.contains(e.target)) {
+      hamburger.checked = false;
+    }
+  });
+
+  // Keyboard support for expand/collapse.
   menuState.addEventListener("keypress", (e) => {
-    if (e.  key === "Enter" || e.key === " ") {
+    if (e.key === "Enter" || e.key === " ") {
       hamburger.checked = !hamburger.checked;
       e.preventDefault();
     }
