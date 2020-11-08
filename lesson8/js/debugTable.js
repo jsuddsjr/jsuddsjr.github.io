4d5e7310fab7dad1644e5213c706e0b449872596
jsuddsjr <jsuddsjr@hotmail.com>
2020-11-08 13:56:03 -0800

    Debug table for form submission

---
+++ lesson8/js/debugTable.js
@@ -0,0 +1,14 @@
+﻿(function (d) {
+  const table = d.querySelector("table.debug");
+  if (table) {
+    const toCell = (v) => `<td>${unescape(v.replace('+',' '))}</td>`;
+    const toRow = (v) => `<tr>${v}</tr>`;
+
+    const htmlValues = window.location.search
+      .substring(1)
+      .split("&")
+      .map((kvp) => toRow(kvp.split("=").map(toCell).join('')))
+      .join('');
+    table.innerHTML += htmlValues;
+  }
+})(document);
