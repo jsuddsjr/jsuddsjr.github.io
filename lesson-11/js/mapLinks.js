9de2db2ceadac1ebf9fd4b923ffe4c83eb1ba1e1
John Sudds <jsuddsjr@hotmail.com>
2020-11-29 22:49:33 -0800

    All maps working

--- lesson-11/js/mapLinks.js
+++ lesson-11/js/mapLinks.js
@@ -2,24 +2,38 @@
   /**
    * @typedef {Object} TownData
    * @property {String} bingMaps
+   * @property {String} bingLevel
    */
+  
+  const locationToken = /{{location}}/g;
+  const levelToken = /{{level}}/g;
+
+  /**
+   *
+   * @param {String} str
+   * @param {TownData} data
+   */
+  const replaceBingData = (str, data) => {
+    return str
+      .replace(locationToken, data.bingMaps)
+      .replace(levelToken, data.bingLevel);
+  };
 
   fetch("data/townData.json")
     .then((response) => response.json())
     .then((data) => {
       /** @type {TownData} */
       const townData = data[d.body.dataset.town || "preston"];
-      const locationToken = "{{location}}";
-      
+
       [
-        (d.getElementById("largeMapLink"), d.getElementById("dirMapLink"))
+        (d.getElementById("largeMapLink"), d.getElementById("dirMapLink")),
       ].forEach(
         /** @param {HTMLAnchorElement} a*/ (a) => {
-          a.href = a.href.replace(locationToken, townData.bingMaps);
+          a.href = replaceBingData(a.href, townData);
         }
       );
       /** @type {HTMLIFrameElement} */
       const iframe = d.getElementById("bingMapsFrame");
-      iframe.src = iframe.src.replace(locationToken, townData.bingMaps);
+      iframe.src = replaceBingData(iframe.src, townData);
     });
 })(document);
