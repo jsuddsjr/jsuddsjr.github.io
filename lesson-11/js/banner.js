1a06156457825f020adf3dd0a392146e1d43094a
John Sudds <jsuddsjr@hotmail.com>
2020-12-01 20:20:25 -0800

    Fish haven banner

--- lesson-11/js/banner.js
+++ lesson-11/js/banner.js
@@ -1,13 +1,25 @@
-(function () {
+(function (d) {
   const FRIDAY = 5;
+  const DECEMBER = 11;
+
+  let showBanner = false;
 
   /** @type {HTMLDivElement} */
   const banner = document.querySelector(".banner-box");
-  if (new Date().getDay() === FRIDAY) {
+  if (banner) {
+    const today = new Date();
+    showBanner =
+      (d.body.dataset.town === "preston" && today.getDay() === FRIDAY) ||
+      (d.body.dataset.town === "fish-haven" &&
+        today.getMonth() === DECEMBER &&
+        today.getDate() <= 12);
+  }
+
+  if (showBanner) {
     banner.style.display = "flex";
     setTimeout(() => banner.classList.add("show"), 1200);
+    banner.querySelector("button").addEventListener("click", () => {
+      banner.classList.remove("show");
+    });
   }
-  banner.querySelector("button").addEventListener("click", () => {
-    banner.classList.remove("show");
-  });
-})();
+})(document);
