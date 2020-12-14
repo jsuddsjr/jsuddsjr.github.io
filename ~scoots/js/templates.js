4b507165855152acba3934ce4e6fbc0f2c43e89e
John Sudds <jsuddsjr@hotmail.com>
2020-12-13 22:35:42 -0800

    Menu template

--- ~scoots/js/templates.js
+++ ~scoots/js/templates.js
@@ -1,12 +1,20 @@
 (function (d) {
   /**
+   * Convert text to document fragment
+   * @param {String} text 
+   */
+  const asFragment = (text) => {
+    return d.createRange().createContextualFragment(text);
+  };
+
+  /**
    * Insert template HTML inside specified element
    * @param {String} selector
    * @param {String} template
    */
   const insertTemplate = async (selector, template = null) => {
     template = template || selector;
-    
+
     /** @type Element */
     const el = d.querySelector(selector);
     if (!el) return false;
@@ -14,16 +22,18 @@
     const response = await fetch(`${template}.template.html`);
     const text = await response.text();
 
-    // Force execute script tags.
-    const range = d.createRange();
-    const fragment = range.createContextualFragment(text);
-    el.appendChild(fragment);
+    // Parse text as HTML
+    const parser = new DOMParser();
+    const html = parser.parseFromString(text, "text/html");
+
+    el.parentElement.replaceChild(asFragment(html.body.innerHTML), el);
+    d.head.appendChild(asFragment(html.head.innerHTML));
 
     return true;
   };
 
   // Nav depends on header.
-  insertTemplate("header").then(() => insertTemplate("nav"));
+  insertTemplate("header").then(() => insertTemplate("nav", "menu"));
   insertTemplate("div.overlay", "weather");
   insertTemplate("footer");
 })(document);
