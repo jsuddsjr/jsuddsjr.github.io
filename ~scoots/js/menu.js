c3b91efb46f7051fcd0d48a15518d2a0f95f0619
John Sudds <jsuddsjr@hotmail.com>
2020-12-14 20:04:47 -0800

    Mini menu

--- ~scoots/js/menu.js
+++ ~scoots/js/menu.js
@@ -7,17 +7,17 @@
   }
 
   const navList = d.querySelector("nav ul");
-  const menuState = d.getElementById("menu-state");
+  const menuRoot = d.querySelector("nav");
 
   const toggleMenu = (e) => {
     const isExpanded = navList.classList.toggle(expanded);
-    menuState.setAttribute("aria-expanded", isExpanded);
+    menuRoot.setAttribute("aria-expanded", isExpanded);
     e.preventDefault();
   };
 
   const collapseMenu = (e) => {
     navList.classList.remove(expanded);
-    menuState.setAttribute("aria-expanded", "false");
+    menuRoot.setAttribute("aria-expanded", "false");
   };
 
   // Selecting a menu item closes the menu.
@@ -36,11 +36,11 @@
   );
 
   // Toggle menu state on click.
-  menuState && menuState.addEventListener("click", toggleMenu);
+  menuRoot && menuRoot.addEventListener("click", toggleMenu);
 
   // Collapse menu if focus moves elsewhere.
   d.body.addEventListener("focusin", (e) => {
-    if (e.target !== menuState && !navList.contains(e.target)) {
+    if (!menuRoot.contains(e.target)) {
       collapseMenu();
     }
   });
@@ -75,30 +75,49 @@
               )
               .join("");
 
-            const subMenuOptions = ul.querySelectorAll("li > a");
+            const subMenuOptions = [...ul.querySelectorAll("li > a")];
             if (subMenuOptions.length) ul.style.display = "block";
 
-            /** Toggle the menu with the keyboard.
-             *  @param {KeyboardEvent} e
+            /**
+             * 
+             * @param {Boolean} toggle
+             */
+            const toggleSubMenu = (toggle) => {
+              menuAnchor.setAttribute("aria-expanded", toggle.toString());
+              subMenuOptions.forEach(
+                /** @param {HTMLAnchorElement} a */ (a) =>
+                  (a.tabIndex = toggle ? 0 : -1)
+              );
+            };
+
+            /**
+             * Toggle the menu with the keyboard.
+             * @param {KeyboardEvent} e
              */
             const handleKey = (e) => {
               switch (e.code) {
                 case "ArrowDown":
-                case "ArrowUp":
                 case "Enter":
                 case "Space":
-                  const toggle = menuAnchor.getAttribute("aria-expanded") !== "true";
-                  menuAnchor.setAttribute("aria-expanded", toggle.toString());
-                  subMenuOptions.forEach(
-                    /** @param {HTMLAnchorElement} a */ (a) =>
-                      (a.tabIndex = toggle ? 0 : -1)
-                  );
+                  const toggle =
+                    menuAnchor.getAttribute("aria-expanded") !== "true";
+                  toggleSubMenu(toggle);
                   e.preventDefault();
+                  break;
+                case "ArrowUp":
+                case "ArrowLeft":
+                case "ArrowRight":
+                  toggleSubMenu(false);
                   break;
               }
             };
 
             menuAnchor.addEventListener("keydown", handleKey);
+            d.addEventListener('focusin', e => {
+              if (e.target !== menuAnchor && !subMenuOptions.includes(e.target)) {
+                toggleSubMenu(false);
+              }
+            });
           }
         );
       }
