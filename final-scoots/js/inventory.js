802267146b78b5b7de69929768f74f70963d1a74
John Sudds <jsuddsjr@hotmail.com>
2020-12-15 20:14:09 -0800

    Reserve selection

--- final-scoots/js/inventory.js
+++ final-scoots/js/inventory.js
@@ -1,6 +1,9 @@
 (function (d) {
   const container = d.querySelector("div.rentals");
 
+  /** @type {HTMLDataListElement} */
+  const options = d.querySelector("datalist#inventory");
+
   /** @typedef {Object} InventoryData
    * @property {Object} cost
    * @property {String} description
@@ -9,11 +12,11 @@
    * @property {String} type
    */
 
-   /**
-    * Convert image base into picture HTML.
-    * @param {String} imageBase 
-    * @param {String} alt 
-    */
+  /**
+   * Convert image base into picture HTML.
+   * @param {String} imageBase
+   * @param {String} alt
+   */
   const formatImage = (imageBase, alt) => {
     const set = [800, 400]
       .map(
@@ -23,28 +26,80 @@
           }px)" srcset="images/${imageBase}-${size}h.webp" >`
       )
       .join("");
-    const img = `<img src="images/${imageBase}-200h.webp" alt="${alt}"/>`
+    const img = `<img src="images/${imageBase}-200h.webp" alt="${alt}"/>`;
     return `<picture>${set}${img}</picture>`;
+  };
+
+  const invByType = {};
+  const invById = {};
+
+  const switchInventory = (value) => {
+    if (!value) return;
+
+    const inventory = invByType[value]
+      ? d.getElementById(invByType[value].imageBase)
+      : d.getElementById(value);
+
+    if (inventory) {
+      d.querySelectorAll(".inventory").forEach(
+        (el) => (el.style.display = "none")
+      );
+      inventory.style.display = "block";
+    }
+
+    if (options && invById[value]) {
+      options.parentElement.children[0].value = invById[value].type;
+    }
   };
 
   fetch("data/inventory.json")
     .then((response) => response.json())
     .then(
       /** @param {{inventory:Array<InventoryData>} data */
-      (data) =>
+      (data) => {
         data.inventory.forEach((inv) => {
-          console.log(inv);
+          invByType[inv.type] = inv;
+          invById[inv.imageBase] = inv;
 
-          const bookmark = d.createElement("A");
-          bookmark.name = inv.imageBase;
-          container.appendChild(bookmark);
+          if (container) {
+            const bookmark = d.createElement("A");
+            bookmark.name = inv.imageBase;
+            container.appendChild(bookmark);
 
-          const div = d.createElement("DIV");
-          
-          div.className = "inventory";
-          div.innerHTML = formatImage(inv.imageBase, inv.type);
+            const div = d.createElement("DIV");
+            div.className = "inventory";
+            div.id = inv.imageBase;
 
-          container.appendChild(div);
-        })
-    );
+            div.innerHTML = formatImage(inv.imageBase, inv.type);
+
+            if (options) {
+                div.innerHTML += `<a class="reservation" href="reservations.html#${inv.imageBase}">Reserve Now</a>`;
+            }
+
+            container.appendChild(div);
+          }
+
+          if (options) {
+            options.innerHTML += `<option id="${inv.imageBase}">${inv.type}</option>`;
+          }
+        });
+
+        // Select the correct item.
+        switchInventory(d.location.hash.substring(1));
+      }
+    )
+    .then(() => container.dispatchEvent(new Event("inventoryLoaded")));
+
+  if (options && container) {
+    /** @type {HTMLInputElement} */
+    const input = options.parentElement.children[0];
+    input.addEventListener("change", (e) => {
+      switchInventory(e.target.value);
+    });
+  }
+
+  window.addEventListener("hashchange", (e) => {
+    var hash = e.target.location.hash.substring(1);
+    switchInventory(hash);
+  });
 })(document);
