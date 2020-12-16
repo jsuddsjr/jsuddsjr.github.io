8f44d93d4ca704072ed52794569173b39ec19414
John Sudds <jsuddsjr@hotmail.com>
2020-12-15 22:00:21 -0800

    Document date

--- final-scoots/js/updateTime.js
+++ final-scoots/js/updateTime.js
@@ -9,22 +9,22 @@
 
     const fullDateOptions = {
       ...dateOptions,
-      weekday: undefined,
       hour: "numeric",
       minute: "numeric",
+      second: "numeric"
     };
 
     const docDate = new Date(document.lastModified);
-    const year = document.getElementById("year");
+    const year = document.getElementById("document-year");
     if (year) year.textContent = docDate.getFullYear();
 
     // Get date elements from Intl object, and convert array to object keys.
-    const p = new Intl.DateTimeFormat("default", dateOptions)
-      .formatToParts()
+    const p = new Intl.DateTimeFormat("default", fullDateOptions)
+      .formatToParts(docDate)
       .reduce((p, c) => ((p[c.type] = c.value), p), {});
 
     // Format date according to homework requirements.
-    const date = document.getElementById("date");
+    const date = document.getElementById("document-date");
     if (date) date.textContent = `${p.weekday}, ${p.day} ${p.month}, ${p.year}`;
   };
 })();
