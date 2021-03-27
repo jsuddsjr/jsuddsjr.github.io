a1816269277da5b527b49d992731ddcc3a4b7e11
John Sudds <jsudds@microsoft.com>
2021-03-26 21:15:24 -0700

    Move to parcelCSS

--- WDD331/W12/parcelCSS/build/styles.5c8f3eab.js
+++ WDD331/W12/parcelCSS/build/styles.5c8f3eab.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\..\\node_modules\\font-awesome\\fonts\\fontawesome-webfont.eot":[["fontawesome-webfont.80db1567.eot","../node_modules/font-awesome/fonts/fontawesome-webfont.eot"],"../node_modules/font-awesome/fonts/fontawesome-webfont.eot"],"./..\\..\\node_modules\\font-awesome\\fonts\\fontawesome-webfont.woff2":[["fontawesome-webfont.cda54bb3.woff2","../node_modules/font-awesome/fonts/fontawesome-webfont.woff2"],"../node_modules/font-awesome/fonts/fontawesome-webfont.woff2"],"./..\\..\\node_modules\\font-awesome\\fonts\\fontawesome-webfont.woff":[["fontawesome-webfont.c3cf7ef2.woff","../node_modules/font-awesome/fonts/fontawesome-webfont.woff"],"../node_modules/font-awesome/fonts/fontawesome-webfont.woff"],"./..\\..\\node_modules\\font-awesome\\fonts\\fontawesome-webfont.ttf":[["fontawesome-webfont.cf80d36a.ttf","../node_modules/font-awesome/fonts/fontawesome-webfont.ttf"],"../node_modules/font-awesome/fonts/fontawesome-webfont.ttf"],"./..\\..\\node_modules\\font-awesome\\fonts\\fontawesome-webfont.svg":[["fontawesome-webfont.9ab71dbb.svg","../node_modules/font-awesome/fonts/fontawesome-webfont.svg"],"../node_modules/font-awesome/fonts/fontawesome-webfont.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51201" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63011" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- WDD331/W12/parcelCSS/src/js/index.js
+++ WDD331/W12/parcelCSS/src/js/index.js
@@ -1,22 +1,23 @@
-// import $ from 'jquery';
-// import popper from 'popper.js';
-// import bootstrap from 'bootstrap';
-// check to see if javascript is working
-function component() {
-  const element = document.createElement('p');
+import $ from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
 
-  element.innerHTML = 'Setup Appears to be working &#128521;';
+// // check to see if javascript is working
+// function component() {
+//   const element = document.createElement('p');
 
-  return element;
-}
-document.body.appendChild(component());
+//   element.innerHTML = 'Setup Appears to be working &#128521;';
 
-// test jquery and Bootstrap setup
-$(document).ready(() => {
-  const element = $('<p></p>').html('Jquery Appears to be working &#128521;');
+//   return element;
+// }
+// document.body.appendChild(component());
 
-  element.appendTo('body');
-  $(() => {
-    $('[data-toggle="popover"]').popover();
-  });
-});
+// // test jquery and Bootstrap setup
+// $(document).ready(() => {
+//   const element = $('<p></p>').html('Jquery Appears to be working &#128521;');
+
+//   element.appendTo('body');
+//   $(() => {
+//     $('[data-toggle="popover"]').popover();
+//   });
+// });
