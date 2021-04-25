0c4f0d9a61a10df4446fecabccc94ab6bef4f461
John Sudds <jsudds@microsoft.com>
2021-04-24 17:01:53 -0700

    Dynamic load for WDD330

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -4,28 +4,110 @@
  * @property {String} label   Description of resource
  */
 
-(function (d) {
+/**
+ * @typedef {Object} WeeklyReport
+ * @property {LinkEntry[]} links
+ * @property {String[]} questions
+ */
+
+Number.prototype[Symbol.iterator] = function* () {
+  for (let i = 1; i <= this; i++) {
+    yield i;
+  }
+};
+
+Number.prototype.pad = function (size) {
+  var s = String(this);
+  return "0".repeat(size - s.length) + s;
+};
+
+(async function (d) {
   /** @type HTMLOListElement */
-  const list = d.querySelector(".js-links-list");
+  const ol = d.querySelector(".js-links");
 
-  /** @type Array<LinkEntry> */
-  const links = [
-    {
-      url: "W01/story_editor.html",
-      label: "Story Editor (from Doing Stuff with Web Things)",
-    },
-  ];
+  /**
+   * Attempts to open the _index.json_ file for the specified week.
+   * If the file cannot be opened, a default set of data is returned.
+   * This code reads the _data-weeks_ attr set on the parent OL element
+   * to determine how many default weeks to return.
+   * @returns Fetched data
+   */
+  const getWeeklyReport = async () => {
+    try {
+      const week = location.search.substr(1);
+      const request = await fetch(`./${week}/index.json`);
+      /** @type WeeklyReport */
+      const links = await request.json();
+      return links;
+    } catch (error) {
+      const weeks = parseInt(ol.dataset["weeks"] || 1);
+      return {
+        links: [...weeks].map((n) => {
+          return { url: "index.html?W" + n.pad(2), label: `Week ${n}` };
+        }),
+        questions: [],
+      };
+    }
+  };
 
-  for (let entry of links) {
+  /**
+   * Convert text to document fragment
+   * @param {String} text
+   */
+  const asFragment = (text) => {
+    return d.createRange().createContextualFragment(text);
+  };
+
+  /**
+   * Insert template HTML inside specified element
+   * @param {String} selector
+   * @param {String} template
+   */
+  const insertTemplate = async (selector, template = null) => {
+    template = template || selector;
+
+    /** @type Element */
+    const el = d.querySelector(selector);
+    if (!el) return false;
+
+    const response = await fetch(`${template}.template.html`);
+    const text = await response.text();
+
+    // Parse text as HTML
+    const parser = new DOMParser();
+    const html = parser.parseFromString(text, "text/html");
+
+    el.parentElement.replaceChild(asFragment(html.body.innerHTML), el);
+    d.head.appendChild(asFragment(html.head.innerHTML));
+
+    return true;
+  };
+
+  const report = await getWeeklyReport();
+
+  // Create list item for each link.
+  for (let entry of report.links) {
     /** @type HTMLListItemElement */
     const li = d.createElement("LI");
     /** @type HTMLAnchorElement */
     const a = d.createElement("A");
     a.href = entry.url;
     a.innerText = entry.label;
-    a.target = "_blank";
     li.appendChild(a);
-    list.appendChild(li);
+    ol.appendChild(li);
   }
 
+  // Append questions, if any.
+  if (
+    report.questions?.length &&
+    (await insertTemplate("#questions", "questions"))
+  ) {
+    const olq = d.querySelector(".js-questions");
+    for (let q of report.questions) {
+      /** @type HTMLListItemElement */
+      const li = d.createElement("LI");
+      li.innerText = q;
+      olq.appendChild(li);
+    }
+  }
 })(document);
