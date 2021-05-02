a8695b0261cb8e65054d5bc9f33d98a0ccc79611
John Sudds <jsudds@microsoft.com>
2021-05-01 18:45:49 -0700

    More JS syntax

--- WDD330/W02/js/run.js
+++ WDD330/W02/js/run.js
@@ -5,12 +5,13 @@ const runCode = (code) => {
   output = document.getElementById("output");
   let result;
   for (let line of code) {
+    if (line.startsWith('//')) {
+      output.innerHTML += `<br/><code>${line}</code><br/>`;
+      continue;
+    }
+
     try {
-      if (line.startsWith('//')) {
-        result = line;
-      } else {
-        result = eval(line);
-      }
+      result = eval(line);
       if (typeof result === "string") {
         result = `'${result}'`;
       } else if (typeof result !== "undefined") {
