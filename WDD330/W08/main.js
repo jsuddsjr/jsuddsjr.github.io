d6e6ca6fea6d9fba185ce120f3d72315eb205017
John Sudds <jsudds@microsoft.com>
2021-06-10 11:11:18 -0700

    Reformat

--- WDD330/W08/main.js
+++ WDD330/W08/main.js
@@ -26,19 +26,18 @@
  * @property {Person[]} results
  */
 
-const baseUrl = 'https://swapi.dev/api/people';
+const baseUrl = "https://swapi.dev/api/people";
 getPersonData(baseUrl);
 
 function setButtonUrl(buttonId, url) {
-    /** @type {HTMLButtonElement} */
-    const button = document.getElementById(buttonId);
-    if (url) {
-        button.disabled = false;
-        button.onclick =() => getPersonData(url)
-    }
-    else {
-        button.disabled = true;
-    }
+  /** @type {HTMLButtonElement} */
+  const button = document.getElementById(buttonId);
+  if (url) {
+    button.disabled = false;
+    button.onclick = () => getPersonData(url);
+  } else {
+    button.disabled = true;
+  }
 }
 
 /**
@@ -46,49 +45,46 @@ function setButtonUrl(buttonId, url) {
  * @param {string} url
  */
 async function getPersonData(url) {
+  const PAGE_SIZE = 10;
+  const page = url.split("page=")[1];
+  const pageNumber = parseInt(page) || 1;
+  const startNumber = (pageNumber - 1) * PAGE_SIZE + 1;
 
-    const PAGE_SIZE = 10;
-    const page = url.split("page=")[1];
-    const pageNumber = parseInt(page) || 1;
-    const startNumber = ((pageNumber - 1) * PAGE_SIZE) + 1;
+  /** @type {PersonData} */
+  const personData = await fetch(url).then((response) => response.json());
 
-    /** @type {PersonData} */
-    const personData = await fetch(url).then(response => response.json());
+  const main = document.querySelector("#main");
+  main.innerHTML = "";
 
-    const main = document.querySelector('#main');
-    main.innerHTML = "";
+  const ol = main.appendChild(document.createElement("ol"));
 
-    const ol = main.appendChild(document.createElement('ol'));
+  const listItems = personData.results.map((p) => `<li><a href="${p.url}">${p.name}</a></li>`);
 
-    const listItems = personData.results.map(p => `<li><a href="${p.url}">${p.name}</a></li>`);
+  ol.innerHTML = listItems.join("");
+  ol.start = startNumber;
 
-    ol.innerHTML = listItems.join('');
-    ol.start = startNumber;
+  [...ol.querySelectorAll("a")].forEach((a) => a.addEventListener("click", handleClick));
 
-    [...ol.querySelectorAll('a')].forEach(a => a.addEventListener('click', handleClick))
-
-    setButtonUrl('next', personData.next);
-    setButtonUrl('prev', personData.previous);
+  setButtonUrl("next", personData.next);
+  setButtonUrl("prev", personData.previous);
 }
 
 async function getPersonDetails(url) {
-    /** @type {PersonData} */
-    const person = await fetch(url).then(response => response.json())
-  
-    const modal = document.querySelector('#main');
-    main.innerHTML = "";
+  /** @type {PersonData} */
+  const person = await fetch(url).then((response) => response.json());
 
-    main.textContent = JSON.stringify(person);
+  const modal = document.querySelector("#main");
+  main.innerHTML = "";
+
+  main.textContent = JSON.stringify(person);
 }
 
 /**
  * @param {Event} event
  */
-function handleClick(event){
-    /** @type {HTMLAnchorElement} */
-    const anchor = event.target;
-    event.preventDefault();
-    getPersonDetails(anchor.href);
+function handleClick(event) {
+  /** @type {HTMLAnchorElement} */
+  const anchor = event.target;
+  event.preventDefault();
+  getPersonDetails(anchor.href);
 }
-
-
