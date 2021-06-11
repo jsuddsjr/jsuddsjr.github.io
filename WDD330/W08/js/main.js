5fe8eadf9b0ca19272a0c0ed483b4729cdbed660
John Sudds <jsudds@microsoft.com>
2021-06-11 14:39:44 -0700

    Move modal to module

--- WDD330/W08/js/main.js
+++ WDD330/W08/js/main.js
@@ -1,53 +1,10 @@
+import Modal from "./modal.js";
 /** @type {import('./pokemon.types.js')} */
 
 (
   function (d) {
     const baseUrl = "https://pokeapi.co/api/v2/pokemon";
-    const modal = d.getElementById("modalBackground");
-
-    // Set up click on close button and modal background to dismiss dialog.
-    [document.getElementById("close"), modal].forEach((el) => el.addEventListener("click", dismissModal));
-
-    [...modal.children].forEach(
-      // Clicks inside the modal do not trigger close.
-      (el) => el.addEventListener("click", (e) => e.stopImmediatePropagation())
-    );
-
-    /**
-     * Show modal dialog with the specified content.
-     * @param {String} title
-     * @param {String|HTMLElement} body
-     * @param {HTMLElement} [triggerElement]
-     */
-    function showModal(title, body, triggerElement) {
-      modal.style.display = "flex";
-      modal.triggerElement = triggerElement;
-
-      const titleElement = modal.querySelector(".modal-title h3");
-      const bodyElement = modal.querySelector(".modal-body");
-      const closeButton = modal.querySelector("button");
-
-      titleElement.textContent = title;
-      if (body instanceof HTMLElement) {
-        bodyElement.innerHTML = "";
-        bodyElement.appendChild(body);
-      } else {
-        bodyElement.textContent = String(body);
-      }
-
-      closeButton.focus();
-    }
-
-    /**
-     * Close dialog and return focus to prior element.
-     */
-    function dismissModal() {
-      modal.style.display = "none";
-      if (modal.triggerElement instanceof HTMLElement) {
-        modal.triggerElement.focus();
-      }
-    }
-
+    const modal = new Modal("modalBackground");
     /**
      *
      * @param {String} buttonId
@@ -69,12 +26,14 @@
      * @param {string} url
      */
     async function getPokemonData(url) {
+      const urlParams = new URLSearchParams(url);
+      const startNumber = urlParams.get("offset") || 1;
+      const pageSize = urlParams.get("limit") || 20;
+
       /** @type {import('./pokemon.types.js').PokemonResponse} */
       const pokemonList = await fetchData(url);
-
       if (pokemonList) {
-        const urlSegments = pokemonList.results[0].url.split("/");
-        const startNumber = parseInt(urlSegments[urlSegments.length - 2]);
+        const totalRecords = pokemonList.count;
 
         const main = d.querySelector("#main");
         main.innerHTML = "";
@@ -116,7 +75,7 @@
       const div = document.createElement("div");
       div.className = "pokemon";
       div.innerHTML = `<img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" />`;
-      showModal(pokemon.name, div, triggerElement);
+      modal.showModal(pokemon.name, div, triggerElement);
     }
 
     /**
