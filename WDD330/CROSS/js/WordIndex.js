56a3f4fd3917d3faa12e7285fb5c5b3a71de7ac4
John Sudds <jsudds@microsoft.com>
2021-06-14 16:54:14 -0700

    WordIndex tests

--- WDD330/CROSS/js/WordIndex.js
+++ WDD330/CROSS/js/WordIndex.js
@@ -5,39 +5,40 @@ const index = new Map();
 /** @type {Map<number, string[]} */
 const indexSize = new Map();
 let loading = false;
+let loaded = false;
+
+const subscribers = new Subscribers(index);
 const LOADED_EVENT = "loaded";
+const FAILED_EVENT = "failed";
 
 const VOWEL = "0";
 const CONSONANT = "1";
 const ANY_TYPE = ".";
+const SHAPE_MATCH = new RegExp(`[${VOWEL}${CONSONANT}${ANY_TYPE}]`);
 const LETTER_MATCH = new RegExp(`[^${VOWEL}${CONSONANT}${ANY_TYPE}]`);
 
 export default class WordIndex {
   constructor() {
-    this.loaded = false;
-    this.subscribers = new Subscribers(this);
-    this.loadIndex();
+    if (!loading && !loaded) {
+      loadIndex().then((loadedOk) => {
+        loaded = loadedOk;
+        subscribers.notify(loadedOk ? LOADED_EVENT : FAILED_EVENT);
+      });
+    }
   }
 
-  async loadIndex() {
-    if (loading) return;
-    loading = true;
+  /**
+   * @returns True, if loaded.
+   */
+  isLoaded = () => loaded;
 
-    const response = await fetch("data/wordshape.txt");
-    const text = await response.text();
-
-    for (let line of text.split(/\r?\n/g)) {
-      const [key, ...words] = line.split(",");
-      index.set(key, words);
-
-      const keyLen = key.length;
-      const sizeArr = indexSize.get(keyLen) || [];
-      if (sizeArr.length === 0) indexSize.set(keyLen, sizeArr);
-      sizeArr.push(key);
-    }
-
-    this.loaded = true;
-    this.subscribers.notify(LOADED_EVENT);
+  /**
+   * JEST: reset global values.
+   */
+  static reset() {
+    index.clear();
+    indexSize.clear();
+    loaded = loading = false;
   }
 
   /**
@@ -50,22 +51,15 @@ export default class WordIndex {
     const types = new Set(shape);
 
     if (types.size > 3 || LETTER_MATCH.test(shape)) {
-      // TODO: Blended search contains letters.
-      return [];
+      const wordMatch = new RegExp("^" + shape.replace(SHAPE_MATCH, "."));
+      const simpleShape = shape.replace(LETTER_MATCH, (sub) => {
+        return /[aeiou]/.test(sub) ? "0" : "1";
+      });
+      return matchShapeSimple(simpleShape).filter((w) => wordMatch.test(w));
     }
 
     if (types.has(ANY_TYPE)) {
-      const result = [];
-      const shapesByLen = indexSize.get(shape.length);
-      if (shapesByLen) {
-        const shapeMatch = new RegExp(shape);
-        shapesByLen.forEach((key) => {
-          if (shapeMatch.test(key)) {
-            result.push.apply(results, index.get(key));
-          }
-        });
-      }
-      return result;
+      return matchShapeSimple(shape);
     }
 
     // Just VOWEL and CONSONANT.
@@ -78,11 +72,64 @@ export default class WordIndex {
    * @returns this
    */
   onLoaded(cb) {
-    if (this.loaded) {
-      cb(this);
+    if (loaded) {
+      cb(index);
     } else {
-      this.subscribers.subscribe(LOADED_EVENT, cb);
+      subscribers.subscribe(LOADED_EVENT, cb);
     }
     return this;
   }
+
+  /**
+   * Get notification if index fails to load.
+   * @param {import("./Subscribers.js").NotifyFunc} cb
+   * @returns this
+   */
+  onFailed(cb) {
+    subscribers.subscribe(FAILED_EVENT, cb);
+    return this;
+  }
+}
+
+/**
+ * Fetch data from server. Can only be called once.
+ */
+async function loadIndex() {
+  if (loading) return;
+  loading = true;
+
+  const response = await fetch("data/wordshape.txt").catch((reason) => {
+    console.log(reason);
+    return false;
+  });
+
+  const text = await response.text();
+
+  for (let line of text.split(/\r?\n/g)) {
+    const [key, ...words] = line.split(",");
+    index.set(key, words);
+
+    const keyLen = key.length;
+    const sizeArr = indexSize.get(keyLen) || [];
+    if (sizeArr.length === 0) indexSize.set(keyLen, sizeArr);
+    sizeArr.push(key);
+  }
+
+  // Enable reload, if needed.
+  loading = false;
+  return true;
+}
+
+function matchShapeSimple(shape) {
+  const results = [];
+  const shapesByLen = indexSize.get(shape.length);
+  if (shapesByLen) {
+    const shapeMatch = new RegExp("^" + shape);
+    shapesByLen.forEach((key) => {
+      if (shapeMatch.test(key)) {
+        results.push.apply(results, index.get(key));
+      }
+    });
+  }
+  return results;
 }
---
+++ WDD330/CROSS/js/__tests__/WordIndex.tests.js
@@ -0,0 +1,38 @@
+import WordIndex from "../WordIndex.js";
+import { describe, expect, it } from "@jest/globals";
+import fetchMockJest from "fetch-mock-jest";
+
+const text = "101,dog,cat,rat\n110,bro,sno,tho\n111,dry,tsk,zzz";
+fetchMockJest.get("data/wordshape.txt", text);
+
+describe("WordIndex", () => {
+  it("should load words", (done) => {
+    WordIndex.reset();
+    const wi = new WordIndex();
+    expect(wi.isLoaded()).toBe(false);
+    wi.onLoaded(() => {
+      expect(wi.isLoaded()).toBe(true);
+      done();
+    });
+  }, 300);
+
+  it("should create index", () => {
+    const wi = new WordIndex();
+    wi.onLoaded((index) => {
+      const words = index.get("111");
+      expect(words.length).toBe(3);
+    });
+  }, 100);
+
+  it("should find matching shapes", () => {
+    const wi = new WordIndex();
+    const words = wi.getWordsByShape("1..");
+    expect(words.length).toBe(9);
+  }, 100);
+
+  it("should find complex shapes", () => {
+    const wi = new WordIndex();
+    const words = wi.getWordsByShape("1.o");
+    expect(words.length).toBe(3);
+  }, 100);
+});
