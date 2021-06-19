96e313cdc8fc63de66b2ca9bb4fe5e9a4ada6924
John Sudds <jsudds@microsoft.com>
2021-06-19 14:21:12 -0700

    JS drum kit

---
+++ WDD330/W09/drums/sounds.js
@@ -0,0 +1,39 @@
+(function (d) {
+  /** @type{NodeListOf<HTMLDivElement>} */
+  const keys = d.querySelectorAll(".key");
+  const audioList = d.querySelectorAll("audio");
+
+  const audioMap = new Map([...audioList].map((el) => [el.dataset.key, el]));
+  const keysMap = new Map([...keys].map((el) => [el.dataset.key, el]));
+
+  d.body.onkeydown = /** @param {KeyboardEvent} event */ (event) => {
+    const code = (event.keyCode || event.which).toString();
+    const audio = audioMap.get(code);
+    if (audio) {
+      audio.currentTime = 0;
+      audio.playbackRate = 1;
+      audio.play();
+      // Let the sound start, then do this...
+      setTimeout(() => {
+        const key = keysMap.get(code);
+        if (key) {
+          key.classList.add("playing");
+          const hits = parseInt(key.dataset.hits) || 10;
+          hits = (hits + 10) % 110;
+          key.style.transform = `translateY(${hits}px)`;
+          key.dataset.hits = hits;
+        }
+      }, 10);
+    }
+  };
+
+  audioList.forEach((ele) => {
+    ele.addEventListener("ended", (event) => {
+      const code = event.target.dataset.key;
+      const key = keysMap.get(code);
+      if (key) {
+        key.classList.remove("playing");
+      }
+    });
+  });
+})(document);
