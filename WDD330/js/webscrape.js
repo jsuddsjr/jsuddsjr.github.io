ad274c9f6d06072b30654238436e84e442fc3a2b
John Sudds <jsudds@microsoft.com>
2021-06-25 11:08:06 -0700

    Big yellow download button

--- WDD330/js/webscrape.js
+++ WDD330/js/webscrape.js
@@ -1,27 +1,36 @@
-const toUser = (el) => {
-  const email = el.querySelector("span.fontSizeMS").textContent;
-  const name = el.querySelector("span.fontWeightSemiBold").textContent;
+let toUser = (el) => {
+  let email = el.querySelector("span.fontSizeMS")?.textContent;
+  let name = el.querySelector("span.fontWeightSemiBold")?.textContent;
   return [email, name];
 };
-const users = new Map(
-  [...document.querySelectorAll(".page-content tbody a")].map(toUser)
-);
 
-const target = document.querySelector(".bolt-page.v-scroll-auto");
+let pageContent = document.querySelector(".page-content");
+let users = new Map([...pageContent.querySelectorAll("tbody a")].map(toUser));
+
+let target = document.querySelector(".bolt-page.v-scroll-auto");
+let btn = document.createElement("button");
+btn.style = "position: fixed; color: black; background-color: yellow; z-index: 99";
+btn.innerText = "Download 0";
+pageContent.insertBefore(btn, pageContent.firstChild);
+
+btn.onclick = () => {
+  let data = [["Email", "Name"], ...users.entries()].map(([key, value]) => `"${key}","${value}"`).join("\n");
+  var anchor = document.createElement("a");
+  anchor.download = "data.csv";
+  var dataBlob = new Blob([data], {
+    type: "application/vnd.ms-excel;charset=UTF-8",
+  });
+  anchor.href = window.URL.createObjectURL(dataBlob);
+  anchor.click();
+};
+
 var observer = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
     if (mutation.addedNodes.length) {
-      const newUsers = [...mutation.addedNodes]
-        .map(toUser)
-        .filter((u1) => !users.has(u1[0]));
+      let newUsers = [...mutation.addedNodes].filter((node) => node instanceof HTMLElement).map(toUser);
       if (newUsers.length) {
         newUsers.forEach((u1) => users.set(...u1));
-        console.log("total", users.size);
-      }
-      // Dump when all have been found.
-      if (users.size === 291) {
-        console.log(users.entries());
-        observer.disconnect();
+        btn.innerText = `Download ${users.size}`;
       }
     }
   });
