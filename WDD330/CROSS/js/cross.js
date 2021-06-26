25abf341a47b6b4514c522a3e135ede1a0cb65ca
John Sudds <jsuddsjr@users.noreply.github.com>
2021-06-26 16:28:48 -0700

    new file

--- WDD330/CROSS/js/cross.js
+++ WDD330/CROSS/js/cross.js
@@ -1,9 +1,9 @@
 import BoardView from "./BoardView.js";
 import WordListView from "./WordListView.js";
 
-const DEFAULT_BOARD_SIZE = 15;
+const DEFAULT_BOARD_SIZE = "15";
 const params = new URLSearchParams(document.location.search);
-const boardSize = parseInt(params.get('size')) || DEFAULT_BOARD_SIZE;
+const boardSize = parseInt(params.get('size') || DEFAULT_BOARD_SIZE);
 
 const board = document.querySelector(".board");
 const across = document.getElementById("across");
@@ -11,12 +11,12 @@ const down = document.getElementById("down");
 const count = document.querySelector('span.count');
 
 if (
-  board instanceof HTMLElement &&
-  across instanceof HTMLElement &&
-  down instanceof HTMLElement &&
-  count instanceof HTMLElement
+    board instanceof HTMLElement &&
+    across instanceof HTMLElement &&
+    down instanceof HTMLElement &&
+    count instanceof HTMLElement
 ) {
-  const boardView = new BoardView(board, boardSize);
-  const wordListView = new WordListView(boardView, across, down, count);
-  boardView.show();
+    const boardView = new BoardView(board, boardSize);
+    const wordListView = new WordListView(boardView, across, down, count);
+    boardView.show();
 }
